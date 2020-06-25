6450a454a06f453c5b63838705419aa039e4d14a
stephenhky <stephenhky@yahoo.com.hk>
2020-06-24 23:23:07 -0400

    another run

--- experiments/run_gpu_tf_dmrg_expt.sh
+++ experiments/run_gpu_tf_dmrg_expt.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-#SBATCH --output=$HOME/tfgpu_dmrg_output.log
-#SBATCH --error=$HOME/tfgpu_dmrg_error.log
+#SBATCH --output=tfgpu_dmrg_output.txt
+#SBATCH --error=tfgpu_dmrg_error.txt
 #SBATCH --partition=gpu
-#SBATCH --gres=gpu:k80:1
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=lscratch:10,gpu:k80:1
 #SBATCH --mem=10g
 
 srun python3 dmrg_expt.py
